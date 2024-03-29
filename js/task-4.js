7929401a6568ac14980e9415832fb2c90a898d10
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-03-29 09:04:36 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form')
 
+formEl.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+
+    const email = formEl.elements.email
+    const emailValue = email.value.trim()
+
+    const password = formEl.elements.password;
+    const passwordValue = password.value.trim()
+
+    if(!emailValue || !passwordValue) {
+        return alert('All form fields must be filled in');
+    } else {
+        const formData = {
+            [formEl.elements.email.name]: [formEl.elements.email.value],
+            [formEl.elements.password.name]: [formEl.elements.password.value],
+        }
+        console.log(formData)
+        formEl.reset()
+    }
+}
