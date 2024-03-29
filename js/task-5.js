0e223c2dd470f03cc3ba8ef613dad2c77ca1cb12
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-03-29 09:19:16 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,14 @@
+const changeBtn = document.querySelector('.change-color')
+changeBtn.addEventListener('click', changeBgColor)
+
+const colorEl = document.querySelector('.color')
+
+function changeBgColor() {
+  let resColor = getRandomHexColor();
+  document.body.style.backgroundColor = resColor;
+  colorEl.innerHTML = resColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
